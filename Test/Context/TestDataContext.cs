bcfe8b24f4d8539c8ff93637506e300fbd91a9b5
math2018avan <math2018avan@gmail.com>
2020-05-21 19:30:52 +0800

    Create TestDataContext.cs
    identity data context added

---
+++ Test/Context/TestDataContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using Test.Models;
+
+namespace Test.Context
+{
+    public class TestDataContext : IdentityDbContext<ApplicationUser>
+    {
+        public TestDataContext(DbContextOptions<TestDataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Country> Countries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Country>()
+                .HasData(
+                    new Country { Id = 1, CountryName = "Singapore", CountryCode = "SG", DateAdded = DateTime.Now, DateModified = DateTime.Now },
+                    new Country { Id = 2, CountryName = "Sri Lanka", CountryCode = "SL", DateAdded = DateTime.Now, DateModified = DateTime.Now }
+                );
+        }
+    }
+}
