8c7ee8500cdd939dc485cdc6c86e61229d68b022
math2018avan <math2018avan@gmail.com>
2020-05-21 19:26:49 +0800

    Create CountryController.cs
    v1 country controller added

---
+++ Test/Controllers/V1/CountryController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace Test.Controllers.V1
+{
+    [ApiVersion("1.0")]
+    public class CountryController : BaseController
+    {
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<IActionResult> GetAllAsync()
+        {
+            var countries = await Context.Countries.ToListAsync();
+            return Ok(countries);
+        }
+    }
+}
