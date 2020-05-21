621f4098fbce6fcc503f9ab09304a585a80e5ce2
math2018avan <math2018avan@gmail.com>
2020-05-21 19:31:54 +0800

    Models
    new entities added

---
+++ Test/Models/Country.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Test.Models
+{
+    public class Country : IEntity
+    {
+        public long Id { get; set; }
+        [Column(TypeName = "varchar(50)")]
+        public string CountryName { get; set; }
+        [Column(TypeName = "varchar(2)")]
+        public string CountryCode { get; set; }
+        public DateTime DateAdded { get; set; }
+        public DateTime DateModified { get; set; }
+    }
+}
---
+++ Test/Models/IEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Test.Models
+{
+    public interface IEntity
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        long Id { get; set; }
+
+        DateTime DateAdded { get; set; }
+
+        DateTime DateModified { get; set; }
+    }
+}
