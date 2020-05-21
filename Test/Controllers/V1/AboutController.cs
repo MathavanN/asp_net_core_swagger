96cf9761d63862da5d903dd8e138a6ac3a8aee79
math2018avan <math2018avan@gmail.com>
2020-05-21 19:26:37 +0800

    Update AboutController.cs
    v1 about controller added

--- Test/Controllers/V1/AboutController.cs
+++ Test/Controllers/V1/AboutController.cs
@@ -1,23 +1,18 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Test.Controllers.V1
 {
-    [ApiController]
-    [Route("api/v{version:apiVersion}/[controller]")]
-    [Produces("application/json")]
     [ApiVersion("1.0")]
     public class AboutController : BaseController
     {
         [HttpGet]
-        public string Get() => "Hello world v1";
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public IActionResult Get() => Ok(new { data = "This is About Details v1" });
 
         [HttpGet]
         [Route("vision")]
-        public string Vision() => "Hello world Vision v1";
-
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public IActionResult Vision() => Ok(new { data = "This is Vision Details v1" });
     }
 }
